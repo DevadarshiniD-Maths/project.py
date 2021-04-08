3e6a49486004a808472bd949e3512e3f79cc28c5
DevadarshiniD-Maths <82138044+DevadarshiniD-Maths@users.noreply.github.com>
2021-04-08 12:31:33 +0530

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,4 @@
+print("To find the area of the circle -")
+Rad=float(input("Enter the radius of the circle: "))
+Area=(22/7)*Rad**2
+print(Area)
---
+++ file and extension.py
@@ -0,0 +1,3 @@
+File_Name=input("Enter the file name with extension :- ")
+A_Split=File_Name.split(".")
+print("The Extension of the file is :- "+(A_Split[-1]))
